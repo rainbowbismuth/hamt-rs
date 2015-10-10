f61a7655f842981ba17574019abb8e3250d2f661
Emily A. Bellows <emily.a.bellows@gmail.com>
2015-10-10 14:13:29 -0700

    added a test using the api with rc

--- tests/strings.rs
+++ tests/strings.rs
@@ -1,5 +1,6 @@
 extern crate hamt;
 use hamt::HamtRc;
+use std::rc::Rc;
 
 #[test]
 fn alter_str() {
@@ -8,4 +9,15 @@ fn alter_str() {
     let mut hamt = HamtRc::<String, String>::new().insert(String::from("hello"), String::from("world"));
     hamt = hamt.alter("hello", |_| Some(String::from("new_world")));
     assert!(hamt["hello"] == "new_world");
+}
+
+#[test]
+fn alter_rc_str() {
+    // TODO: So ugly, should see if I can optimize the ergonomics for reference counted keys/values.
+    let hello = Rc::new(String::from("hello"));
+    let world = Rc::new(String::from("world"));
+    let new_world = Rc::new(String::from("new_world"));
+    let mut hamt = HamtRc::<Rc<String>, Rc<String>>::new().insert(hello.clone(), world.clone());
+    hamt = hamt.alter(&hello, |_| Some(new_world.clone()));
+    assert!(hamt[&hello] == new_world);
 }
